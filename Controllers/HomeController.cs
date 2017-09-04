b3905dff412cd0772f8673f2a36e8e91a20d021c
hauserJr <free576002@gmail.com>
2017-09-05 00:44:54 +0800

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace LiveDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        Test_Method_One _Test_Method_One = new Test_Method_One();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            _Test_Method_One.LiveTest();
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TestMethod/Test_Method_One.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class Test_Method_One
+{
+    public bool LiveTest ()
+    {
+        return true;
+    }
+}
